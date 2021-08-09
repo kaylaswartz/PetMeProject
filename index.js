ad4b9e2f5f478de40ac70bdcd176b7c41b56b8bd
Kayla Swartzlander <swartzlander20@gmail.com>
2021-08-09 15:37:23 -0500

    form logic in index.js

--- index.js
+++ index.js
@@ -1,58 +1,45 @@
-url = 'https://cataas.com/cat?json=true'
-fetch('https://cataas.com/cat?json=true')
-  .then(function(response){
-    console.log(response)
-    return response.json();
-}).then(function(catData){
-    console.log(catData)
-    var catGif = document.getElementById("catImage").src 
-    catGif = catData.url
-    console.log(catGif)
-    return catData
-})
+// when Submit button is clicked
+// if certain answers are selected
+// then display dog gif
+// otherwise
+//display cat gif
+function clickedButton() {
+    let cat = 0;
+    let dog = 0;
+    if (question1.value === "low") {
+        cat++;
+    } else if (question1.value === "medium") {
+        cat++;
+        dog++;
+    } else {
+        dog++;
+    };
+    if (question2.value === "never") {
+        dog++;
+    } else if (question2.value === "sometimes") {
+        cat++;
+        dog++;
+    } else {
+        cat++;
+        console.log("Dog: ", dog);
+        console.log("Cat: ", cat);
+    }
+
+}
 
 
-// then(function(data){
-//     console.log(data)
-//     var arrayOfAvatarClass = document.getElementsByClassName('catgif');
-//     return data
-// })
-// fetch('https://cataas.com/cat/gif')
-//   .then(function(data){
-//     return data
-//     //console.log(data)
-// });
 
-// class getCat{
-//     constructor() {
-//         this.cats = {
-//         }; // or possibly 0
-//         // null ?  probably shouldn't use something we don't know
-//     }
-//     addAnimal(catUrl) {
-//         if (animal.hungry === false) {
-//             return this.field.push(animal);
-//         }
-//     }
-// }
 
-// class Farm {
-//     constructor() {
-//         this.field = []; // or possibly 0
-//         // null ?  probably shouldn't use something we don't know
-//     }
 
-//     // make method
-//     addAnimal(animal) {
-//         if (animal.hungry === false) {
-//             return this.field.push(animal);
-//         }
-//     }
-//     // possibly have a field with arces
 
-//     work() {
-//       // track work done by farmer
-//     }
-// }
-
-// export default Farm;
+    fetch('https://cataas.com/cat?json=true')
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (catData) {
+            console.log(catData)
+            var catGif = document.getElementById("catImage").src
+            catGif = catData.url;
+            console.log(catGif);
+            return catData;
+        })
