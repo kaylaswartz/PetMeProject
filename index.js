b1ac92a64e86a4a088df8ee359655abe99bf8687
Kayla Swartzlander <swartzlander20@gmail.com>
2021-08-17 11:51:22 -0500

    moved form functionality into processForm function

--- index.js
+++ index.js
@@ -18,106 +18,107 @@ function hideButtons() {
     }
 }
 hideButtons();
+function processForm() {
+    let cat = 0;
+    let dog = 0;
+    hideButtons();
 
+    let selectedAnswers = document.querySelectorAll("input:checked") // creating an array with all the checked values in it
+    //evaluating question 1
+
+    if (selectedAnswers[0].value === "low") {
+        cat++;
+    } else if (selectedAnswers[0].value === "medium") {
+        cat++;
+        dog++;
+    } else {
+        dog++;
+    };
+    //evaluating question 2
+    if (selectedAnswers[1].value === "never") {
+        dog++;
+    } else if (selectedAnswers[1].value === "sometimes") {
+        cat++;
+        dog++;
+    } else {
+        cat++;
+    }
+    // evaluating question 3
+    if (selectedAnswers[2].value === "yes") {
+        dog++;
+    } else if (selectedAnswers[2].value === "no") {
+        cat++;
+    }
+    //evaluating question 4
+    if (selectedAnswers[3].value === "0") {
+        cat++;
+        dog++;
+    } else if (selectedAnswers[3].value === "25") {
+        cat++;
+        dog++;
+    } else if (selectedAnswers[3].value === "50") {
+        cat++;
+        dog++;
+    } else if (selectedAnswers[3].value === "75") {
+        cat++;
+        dog++;
+    } else {
+        cat++;
+        dog++;
+    }
+
+    //evaluating question 5
+    if (selectedAnswers[4].value === "NoFur") {
+        cat++;
+    } else if (selectedAnswers[4].value === "LittleFur") {
+        cat++;
+        dog++;
+    } else {
+        dog++;
+    };
+
+    console.log("You are a cat person:", cat)
+    console.log("You are a dog person", dog)
+
+
+    if (cat > dog) {
+        fetch(`https://cataas.com/cat?json=true`)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            }).then(function (catData) {
+                catAPIData = catData
+                let url = catAPIData.url
+                let arrayOfImage = document.getElementById('petImage');
+                arrayOfImage.src = `https://cataas.com/${url}`
+                continueDisplayCat.style.display = 'block';
+
+            })
+    } else if (cat < dog) {
+        fetch(`https://random.dog/woof.json`)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            }).then(function (dogData) {
+                dogAPIData = dogData
+                let url = dogAPIData.url
+
+                let arrayOfImage = document.getElementById('petImage');
+                arrayOfImage.src = url
+                continueDisplayDog.style.display = 'block';
+
+            })
+    } else if (cat === dog) {
+        let arrayOfImage = document.getElementById('petImage')
+        arrayOfImage.src = `llama.jpg`;
+        continueDisplayLlama.style.display = 'block';
+
+    }
+}
 var btn = document.getElementById('submit');// when Submit button is clicked
 if (btn !== null) {
     btn.addEventListener('click', function () {
-        let cat = 0;
-        let dog = 0;
-        hideButtons();
-
-        let selectedAnswers = document.querySelectorAll("input:checked") // creating an array with all the checked values in it
-        //evaluating question 1
-
-        if (selectedAnswers[0].value === "low") {
-            cat++;
-        } else if (selectedAnswers[0].value === "medium") {
-            cat++;
-            dog++;
-        } else {
-            dog++;
-        };
-        //evaluating question 2
-        if (selectedAnswers[1].value === "never") {
-            dog++;
-        } else if (selectedAnswers[1].value === "sometimes") {
-            cat++;
-            dog++;
-        } else {
-            cat++;
-        }
-        // evaluating question 3
-        if (selectedAnswers[2].value === "yes") {
-            dog++;
-        } else if (selectedAnswers[2].value === "no") {
-            cat++;
-        }
-        //evaluating question 4
-        if (selectedAnswers[3].value === "0") {
-            cat++;
-            dog++;
-        } else if (selectedAnswers[3].value === "25") {
-            cat++;
-            dog++;
-        } else if (selectedAnswers[3].value === "50") {
-            cat++;
-            dog++;
-        } else if (selectedAnswers[3].value === "75") {
-            cat++;
-            dog++;
-        } else {
-            cat++;
-            dog++;
-        }
-
-        //evaluating question 5
-        if (selectedAnswers[4].value === "NoFur") {
-            cat++;
-        } else if (selectedAnswers[4].value === "LittleFur") {
-            cat++;
-            dog++;
-        } else {
-            dog++;
-        };
-
-        console.log("You are a cat person:", cat)
-        console.log("You are a dog person", dog)
-
-
-        if (cat > dog) {
-            fetch(`https://cataas.com/cat?json=true`)
-                .then(function (response) {
-                    console.log(response);
-                    return response.json();
-                }).then(function (catData) {
-                    catAPIData = catData
-                    let url = catAPIData.url
-                    let arrayOfImage = document.getElementById('petImage');
-                    arrayOfImage.src = `https://cataas.com/${url}`
-                    continueDisplayCat.style.display = 'block';
-
-                })
-        } else if (cat < dog) {
-            fetch(`https://random.dog/woof.json`)
-                .then(function (response) {
-                    console.log(response);
-                    return response.json();
-                }).then(function (dogData) {
-                    dogAPIData = dogData
-                    let url = dogAPIData.url
-
-                    let arrayOfImage = document.getElementById('petImage');
-                    arrayOfImage.src = url
-                    continueDisplayDog.style.display = 'block';
-
-                })
-        } else if (cat === dog) {
-            let arrayOfImage = document.getElementById('petImage')
-            arrayOfImage.src = `llama.jpg`;
-            continueDisplayLlama.style.display = 'block';
-
-        }
-
+        processForm();
     });
 }
 
